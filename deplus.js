f0882a9da5cb88f720a788776718af2c63c1b212
ananas29 <96879344+ananas29@users.noreply.github.com>
2022-05-24 12:47:10 +0700

    upload deutsch plus
    first website

---
+++ deplus.js
@@ -0,0 +1,24 @@
+const txtElement = ['Satzbau', 'Konnektoren', 'Präpositionen', 'Pronomen', 'usw.'];
+let count = 0;
+let txtIndex = 0;
+let currentTxt = '';
+let words = '';
+
+(function typing(){
+    if(count == txtElement.length){
+        count = 0;
+    }
+
+    currentTxt = txtElement[count];
+
+    words = currentTxt.slice (0, ++txtIndex)
+    document.querySelector('.typing-effect').textContent = words;
+
+    if(words.length == currentTxt.length){
+        count++;
+        txtIndex = 0;
+    }
+
+    setTimeout(typing, 500);
+
+})();
